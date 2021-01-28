4930fc8d34a728431856137b663c730c08e6ebc5
XynanXDB <penpowerslider@gmail.com>
2021-01-29 02:37:00 +0800

    Setup Input infrastructure

---
+++ GGJ2021/Assets/Scripts/Runtime/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using Game.Runtime.Input;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game.Runtime.Player
+{
+    public class Player : MonoBehaviour, RLWControls.IGameActions
+    {
+        [SerializeField] protected InputHandler InputHandler;
+
+        void Awake()
+        {
+            InputHandler.SetGameCallbacks(this);
+        }
+
+        public void OnUpDown(InputAction.CallbackContext context)
+        {
+            Debug.Log(context.ReadValue<float>());
+        }
+
+        public void OnLeftRight(InputAction.CallbackContext context)
+        {
+            Debug.Log(context.ReadValue<float>());
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                Debug.Log("Interact");
+        }
+    }
+}
