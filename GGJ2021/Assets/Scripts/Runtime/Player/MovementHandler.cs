6fffcafaea82c8a31140e70cbdb5f775d9efdef7
XynanXDB <penpowerslider@gmail.com>
2021-01-30 02:54:12 +0800

    Rotation is up

--- GGJ2021/Assets/Scripts/Runtime/Player/MovementHandler.cs
+++ GGJ2021/Assets/Scripts/Runtime/Player/MovementHandler.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Game.Runtime.Player
 {
     public class MovementHandler : MonoBehaviour
     {
+        [Tooltip("Translation")]
         [SerializeField] protected float MovementSpeed = 5.0f;
-        [SerializeField] private Rigidbody Rigidbody;
-        
+        [SerializeField] protected Transform MeshToRotate;
         private Vector3 _MovementVector;
         private Transform TransformToMove;
-        private float IsometricOffsetAngle;
         
         public float HorizontalSpeed
         {
@@ -21,20 +21,23 @@ namespace Game.Runtime.Player
             set { _MovementVector.z = value; }
         }
         
-        void Awake()
-        {
-            
-            TransformToMove = transform;
-        }
+        void Awake() 
+            => TransformToMove = transform;
 
         void Update()
         {
+            if (_MovementVector.sqrMagnitude <= 0.0f) 
+                return;
+                
             Transform T = transform;
             Quaternion OffsetRotation = Quaternion.AngleAxis(45.0f, Vector3.up);
             Vector3 Vertical = OffsetRotation * T.forward * (_MovementVector.z * MovementSpeed * Time.deltaTime);
             Vector3 Horizontal = OffsetRotation * T.right * (_MovementVector.x * MovementSpeed * Time.deltaTime);
+            Vector3 FinalDirection = (Vertical + Horizontal).normalized;
             
             TransformToMove.Translate(Vertical + Horizontal, Space.World);
+
+            MeshToRotate.LookAt(FinalDirection + TransformToMove.position);
         }
     }
 }
