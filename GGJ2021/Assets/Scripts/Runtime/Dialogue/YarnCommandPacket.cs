bf150e841e92f85f714202021521a3e70a9364fb
XynanXDB <penpowerslider@gmail.com>
2021-01-29 22:41:45 +0800

    Adding the foundation for EndingScene with YarnSpinner

---
+++ GGJ2021/Assets/Scripts/Runtime/Dialogue/YarnCommandPacket.cs
@@ -0,0 +1,25 @@
+﻿namespace Game.Runtime.Dialogue
+{
+    public struct YarnCommandPacket
+    {
+        public string Name;
+        public string Action;
+
+        public YarnCommandPacket(string InName, string InAction)
+        {
+            Name = InName;
+            Action = InAction;
+        }
+
+        public override bool Equals(object obj) 
+            => obj is YarnCommandPacket Packet && Name == Packet.Name && Action == Packet.Action;
+
+        public override int GetHashCode() => base.GetHashCode();
+
+        public static bool operator ==(YarnCommandPacket Self, YarnCommandPacket Other) 
+            => Self.Name == Other.Name && Self.Action == Other.Action;
+
+        public static bool operator !=(YarnCommandPacket Self, YarnCommandPacket Other) 
+            => !(Self == Other);
+    }
+}
