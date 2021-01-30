25f842c5400ccfc05c163d11d4b37986f36f3a71
Oatmil <limtaojuen@gmail.com>
2021-01-30 10:03:17 +0800

    Added Slight Tilt when player Rotate Around

--- GGJ2021/Assets/Scripts/Runtime/Player/MovementHandler.cs
+++ GGJ2021/Assets/Scripts/Runtime/Player/MovementHandler.cs
@@ -9,10 +9,17 @@ namespace Game.Runtime.Player
         [Tooltip("Translation")]
         [SerializeField] protected float MovementSpeed = 5.0f;
         [SerializeField] protected Transform MeshToRotate;
+        [SerializeField] protected Transform MeshToAngle;
         private Vector3 _MovementVector;
         private Transform TransformToMove;
         Coroutine LookAtLerpRoutine;
+        Coroutine TiltRoutine;
         Vector3 FinalDirection;
+        float PrevRotationY;
+        float CurrentRotationY;
+        float TiltElapsedTime;
+        bool TiltCW = false;
+        bool TiltCCW = false;
 
         public float HorizontalSpeed
         {
@@ -30,21 +37,55 @@ namespace Game.Runtime.Player
             if (LookAtLerpRoutine == null)
                 LookAtLerpRoutine = StartCoroutine(SmoothRotate());
             FinalDirection = Vector3.left;
+            PrevRotationY = MeshToRotate.localEulerAngles.y;
+            CurrentRotationY = MeshToRotate.localEulerAngles.y;
+            MeshToAngle = MeshToRotate.GetChild(0);
         }
 
         void Update()
         {
-            if (_MovementVector.sqrMagnitude <= 0.0f) 
+            if (_MovementVector.sqrMagnitude <= 0.0f)
+            {
+                TiltCCW = false;
+                TiltCW = false;
                 return;
+            }
                 
             Transform T = transform;
             Quaternion OffsetRotation = Quaternion.AngleAxis(45.0f, Vector3.up);
             Vector3 Vertical = OffsetRotation * T.forward * (_MovementVector.z * MovementSpeed * Time.deltaTime);
             Vector3 Horizontal = OffsetRotation * T.right * (_MovementVector.x * MovementSpeed * Time.deltaTime);
             FinalDirection = (Vertical + Horizontal).normalized;
-            
-            TransformToMove.Translate(Vertical + Horizontal, Space.World);
 
+            CurrentRotationY = MeshToRotate.localEulerAngles.y;
+            TransformToMove.Translate(Vertical + Horizontal, Space.World);
+            // filter for Previous Value due to 180 limit on rotation
+            if (CurrentRotationY < 0 && PrevRotationY > 0)
+                PrevRotationY = -360 + PrevRotationY;
+            else if (CurrentRotationY > 0 && PrevRotationY < 0)
+                PrevRotationY = 360 + PrevRotationY;
+
+            if (PrevRotationY < CurrentRotationY  && !TiltCW)
+            {
+                if (TiltRoutine != null)
+                {
+                    StopCoroutine(TiltRoutine);
+                }
+                TiltCW = true;
+                TiltCCW = false;
+                TiltRoutine = StartCoroutine(TiltMesh(true));
+            }
+            else if (PrevRotationY > CurrentRotationY && !TiltCCW)
+            {
+                if (TiltRoutine != null)
+                {
+                    StopCoroutine(TiltRoutine);
+                }
+                TiltCW = false;
+                TiltCCW = true;
+                TiltRoutine = StartCoroutine(TiltMesh(false));
+            }
+            PrevRotationY = CurrentRotationY;
         }
 
         IEnumerator SmoothRotate()
@@ -53,8 +94,37 @@ namespace Game.Runtime.Player
             {
                 Quaternion ToRotate = Quaternion.LookRotation(FinalDirection, MeshToRotate.up);
                 MeshToRotate.rotation = Quaternion.Lerp(MeshToRotate.rotation, ToRotate, 10 * Time.deltaTime);
+
                 yield return new WaitForEndOfFrame();
             }
+        }
+
+        IEnumerator TiltMesh(bool CW)
+        {
+            TiltElapsedTime = 0;
+            if (CW)
+            {
+                Debug.Log("CW");
+                while (TiltElapsedTime < 1)
+                {
+                    TiltElapsedTime += Time.deltaTime;
+                    MeshToAngle.localEulerAngles = Vector3.Lerp(Vector3.back * 30, Vector3.zero, TiltElapsedTime);
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+            else
+            {
+                Debug.Log("CCW");
+                while (TiltElapsedTime < 1)
+                {
+                    TiltElapsedTime += Time.deltaTime;
+                    MeshToAngle.localEulerAngles = Vector3.Lerp(Vector3.forward * 30, Vector3.zero, TiltElapsedTime);
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+
+            TiltCW = false;
+            TiltCCW = false;
             yield return null;
         }
     }
