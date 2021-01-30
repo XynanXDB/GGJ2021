60ac373e344ccfbcce9e0cc62195f459a5c54ba4
XynanXDB <penpowerslider@gmail.com>
2021-01-31 04:18:58 +0800

    -Added GameInstance for scene management

---
+++ GGJ2021/Assets/Scripts/Runtime/management/GameInstance.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game.Runtime.management
+{
+    public class GameInstance : MonoBehaviour
+    {
+        public static GameInstance UGameInstance;
+
+        void Awake()
+        {
+            if (UGameInstance == null)
+            {
+                UGameInstance = this;
+                DontDestroyOnLoad(this);
+            }
+            else
+                Destroy(this);
+            
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        void OnSceneLoaded(Scene CurrentScene, LoadSceneMode Mode)
+        {
+            
+        }
+    }
+}
