96dc127b022d7336d69356dff340ce34f90be629
Oatmil <limtaojuen@gmail.com>
2021-01-31 20:45:27 +0800

    mat for main level

---
+++ GGJ2021/Assets/Scripts/Runtime/management/LoadNewGame.cs
@@ -0,0 +1,11 @@
+﻿
+using Game.Runtime.management;
+using UnityEngine;
+
+public class LoadNewGame : MonoBehaviour
+{
+    public void GoToMainMenu()
+    {
+        GameInstance.UGameInstance.LoadScene("MainMenu");
+    }
+}
