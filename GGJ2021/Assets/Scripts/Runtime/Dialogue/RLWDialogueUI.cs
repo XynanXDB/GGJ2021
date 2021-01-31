f50c1580c6864a7dde0b6ab9bc32ecb9e5d78539
XynanXDB <penpowerslider@gmail.com>
2021-01-31 17:39:26 +0800

    Update

--- GGJ2021/Assets/Scripts/Runtime/Dialogue/RLWDialogueUI.cs
+++ GGJ2021/Assets/Scripts/Runtime/Dialogue/RLWDialogueUI.cs
@@ -9,15 +9,30 @@ namespace Game.Runtime.Dialogue
         [SerializeField] protected TMP_Text SpeechText;
         [SerializeField] protected DialogueSpeakerVisuals SpeakerVisuals;
         [SerializeField] protected GameObject Speaker1;
+        [SerializeField] protected GameObject WinScene;
+        [SerializeField] protected GameObject LoseScene;
         
         void Start()
         {
             onLineUpdate.AddListener(UpdateSpeechText);
+            onDialogueEnd.AddListener(OnDialogueEnd);
                 
             ITalkable Player = GameObject.FindGameObjectWithTag("Player").GetComponent<ITalkable>();
             Player.SendNativeCommand(new []{Player.GetSpeakerInfo().InternalIdentifier, "InputModeUI"});
             
             SpeakerVisuals.SetupReference(Speaker1.GetComponent<ITalkable>());
+        }
+
+        void OnDialogueEnd()
+        {
+            float FinalScore = DialogueManager.UDialogueManager.GetFinalScore();
+            
+            if (FinalScore >= 0.35f)
+                WinScene.SetActive(true);
+            else
+                LoseScene.SetActive(true);
+            
+            gameObject.SetActive(false);
         }
 
         void UpdateSpeechText(string Texts)
--- GGJ2021/Assets/Scripts/Runtime/management/GameManager.cs
+++ GGJ2021/Assets/Scripts/Runtime/management/GameManager.cs
@@ -69,15 +69,6 @@ public class GameManager : MonoBehaviour
     {
         if (m_instance == null)
             m_instance = this;
-        
-        playerObject.SetInputMode(InputMode.Disable);
-
-        ProfileOKButton.onClick.AddListener(OnClickProfileOK);
-        ProfilePrompt.SetActive(true);
-        
-        CueCardText.gameObject.SetActive(false);
-        InGameHUD.SetActive(false);
-        GeneratorGFProfile();
     }
 
     void OnClickProfileOK()
@@ -90,6 +81,15 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         PostInteractNotification += UIManager.UUIManager.PostInteractNotification;
+        
+        playerObject.SetInputMode(InputMode.Disable);
+
+        ProfileOKButton.onClick.AddListener(OnClickProfileOK);
+        ProfilePrompt.SetActive(true);
+        
+        CueCardText.gameObject.SetActive(false);
+        InGameHUD.SetActive(false);
+        GeneratorGFProfile();
         
         // Temporary need link this to a reset
         StartGame();
